1aa8787ae276b79accc523a709f36c7421dc9494
drayl <sunrise.161@mail.ru>
2022-06-21 21:48:13 +0300

    removed ContextModule and added builder interface for ApplicationComponent

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.dependencyinjectionstart.example2.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dependencyinjectionstart.R
-import com.example.dependencyinjectionstart.example2.di.ContextModule
 import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
 import javax.inject.Inject
 
@@ -11,10 +10,8 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var viewModel: ExampleViewModel
-    val component by lazy {
-        DaggerApplicationComponent.builder()
-            .contextModule(ContextModule(application))
-            .build()
+    private val component by lazy {
+        DaggerApplicationComponent.builder().context(application).build()
     }
 
         override fun onCreate(savedInstanceState: Bundle?) {
