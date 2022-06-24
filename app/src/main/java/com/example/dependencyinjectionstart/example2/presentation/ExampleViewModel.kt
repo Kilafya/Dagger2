2ca36a1ab306f4c8724727b77043eb8587ac5a4f
drayl <sunrise.161@mail.ru>
2022-06-24 21:19:34 +0300

    Added subcomponent

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel.kt
@@ -6,11 +6,12 @@ import com.example.dependencyinjectionstart.example2.domain.ExampleUseCase
 import javax.inject.Inject
 
 class ExampleViewModel @Inject constructor(
-    private val useCase: ExampleUseCase
+    private val useCase: ExampleUseCase,
+    private val id: String
 ): ViewModel() {
 
     fun method() {
-        Log.d("ViewModel", "$this")
+        Log.d("ViewModel", "$this id - $id")
         useCase.invoke()
     }
 }
